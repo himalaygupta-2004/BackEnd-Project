a9bfd7edab5e69cf88101795aeda93b87c787669
Himalay Gupta <141752020+himalaygupta-2004@users.noreply.github.com>
2024-08-18 23:39:24 +0530

    file upload configuration setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { url } from "inspector";
+
+//files cant be deleted so they are unliked
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return "couldnt find the filePath";
+    //upload on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log(response.url);
+    console.log("File has been uploaded successfully on cloudinary");
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    //remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
